89c8d46f416c286e8316b445e5846582f93632e8
RomanPrikaztsikov <mikolkyle@gmail.com>
2025-01-27 13:33:42 +0200

    Add files via upload

--- Praktiline_too_Lihtsad_prgrammid.py
+++ Praktiline_too_Lihtsad_prgrammid.py
@@ -2,6 +2,89 @@ from datetime import *
 from calendar import *
 from math import *
 
+#10
+try:
+    minutid=int(input("Sisesta aeg minutites: "))
+    if minutid<=0:
+        print("Vale arv!")
+    else:
+        tunnid=minutid//60
+        jääk_minutid=minutid % 60
+        print(f"{minutid} minutit on {tunnid},{jääk_minutid}")
+
+except:
+    print("Palun sisesta täisarv!")
+
+
+
+#9
+kiirus_kmh=29.9
+meetrit_per_minut=(kiirus_kmh * 1000)/60
+print(f"Rulluisutaja läbib ühe minutiga {meetrit_per_minut} meetrit")
+
+
+#8
+try:
+    kütus=float(input("Sisesta kütusekogus liitrites: "))
+    kilomeetrid=float(input("Sisesta läbitud kilomeetrid: "))
+    if kütus <=0 or kilomeetrid <=0:
+        print("Vale numbrid! Nad peavad olema positivseed.")
+    else:
+        kütusekulu_100km = (kütus / kilomeetrid)*100
+        print(f"Kütusekulu 100 km kohta on: {kütusekulu_100km} liitrit")
+
+except:
+    print("Vale andmed!.")
+
+
+
+#7
+try:
+    a=float(input("a: "))
+    b=float(input("b: "))
+    if a>0 and b>0:
+        print("Arvutamine")
+        S=a*b
+        P=(a+b)*2
+        print(f"S={S}, P={P}")
+    else:
+         print("Arvud peavad suurem kui 0 olla!")
+except:
+    print("Vale andmed!")
+    
+
+
+
+# #6
+# print('''Rong see sõitis tsuhh tsuhh tsuhh,
+# piilupart oli rongijuht.
+# Rattad tegid rat tat taa,
+# rat tat taa ja tat tat taa.
+# Aga seal rongi peal,
+# kas sa tead, kes olid seal?
+
+# Rong see sõitis tuut tuut tuut,
+# piilupart oli rongijuht.
+# Rattad tegid kill koll koll,
+# kill koll koll ja kill koll kill.''').capitalize()
+
+
+
+
+#5
+a="kill-koll ".capitalize()
+b="killadi-koll ".capitalize()
+print(a*2,b,a*2,b,a*4)
+
+#4
+d=2.575
+maaR=6378
+maaR*=100000
+Pmaa=2*pi*maaR
+kogus=Pmaa/d
+print(f"Mel on vaja {int(kogus):,d} mündi. ")
+print(f"Mel on vaja {int(kogus*2):,d} euro. ")
+
 #"Ülesanne 3"
 try:
    R=float(input("R: "))
